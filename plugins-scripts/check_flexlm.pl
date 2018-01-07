321eedcc1441e5e50a12c1bbe53dd3e2ba60dbec
William West <william.a.west@gmail.com>
2018-01-07 00:14:52 -0800

    Add unmodified yale fork of check_flexlm from 2011

--- plugins-scripts/check_flexlm.pl
+++ plugins-scripts/check_flexlm.pl
@@ -1,4 +1,4 @@
-#!@PERL@ -w
+#! /usr/bin/perl -w
 #
 # usage: 
 #    check_flexlm.pl license_file
@@ -9,7 +9,7 @@
 # Check and interpret the output of lmstat
 # and create returncodes and output.
 #
-# Contrary to most other plugins, this script takes
+# Contrary to the nagios concept, this script takes
 # a file, not a hostname as an argument and returns
 # the status of hosts and services described in that
 # file. Use these hosts.cfg entries as an example
@@ -31,20 +31,25 @@
 # srigler@marathonoil.com,cerice@marathonoil.com
 
 
-
 use strict;
 use Getopt::Long;
-use vars qw($opt_V $opt_h $opt_F $opt_t $verbose $PROGNAME);
-use FindBin;
-use lib "$FindBin::Bin";
+use vars qw($opt_V $opt_h $opt_F $opt_t $opt_w $opt_c $verbose $PROGNAME);
+use lib "/usr/local/nagios/libexec" ;
 use utils qw(%ERRORS &print_revision &support &usage);
+use File::Basename;
+use Date::Manip;
 
 $PROGNAME="check_flexlm";
+
+use constant {
+    WARN => 30,
+    CRIT => 0,
+};
 
 sub print_help ();
 sub print_usage ();
 
-$ENV{'PATH'}='@TRUSTED_PATH@';
+#$ENV{'PATH'}='';
 $ENV{'BASH_ENV'}=''; 
 $ENV{'ENV'}='';
 
@@ -54,33 +59,51 @@ GetOptions
 	 "h"   => \$opt_h,   "help"       => \$opt_h,
 	 "v"   => \$verbose, "verbose"    => \$verbose,
 	 "F=s" => \$opt_F,   "filename=s" => \$opt_F,
-	 "t=i" => \$opt_t, "timeout=i"  => \$opt_t);
+	 "t=i" => \$opt_t, "timeout=i"  => \$opt_t,
+	 "w=i" => \$opt_w, "warning=i"  => \$opt_w,
+	 "c=i" => \$opt_c, "critical=i"  => \$opt_c);
 
 if ($opt_V) {
-	print_revision($PROGNAME,'@NP_VERSION@');
-	exit $ERRORS{'UNKNOWN'};
+	print_revision($PROGNAME,'0.2 (Yale fork)');
+	exit $ERRORS{'OK'};
 }
 
 unless (defined $opt_t) {
 	$opt_t = $utils::TIMEOUT ;	# default timeout
 }
 
+unless (defined $opt_w) {
+	$opt_w = WARN ;	                # default warn (days)
+}
 
-if ($opt_h) {print_help(); exit $ERRORS{'UNKNOWN'};}
+unless (defined $opt_c) {
+	$opt_c = CRIT ;	                # default crit (days)
+}
+
+# doesn't make sense to have $opt_w < $opt_c
+if ( $opt_w < $opt_c ) {
+	print "Warn threshold $opt_w is less than crit threshold $opt_c\n";
+	exit $ERRORS{'UNKNOWN'};
+}
+
+
+if ($opt_h) {print_help(); exit $ERRORS{'OK'};}
 
 unless (defined $opt_F) {
 	print "Missing license.dat file\n";
 	print_usage();
 	exit $ERRORS{'UNKNOWN'};
 }
-# Just in case of problems, let's not hang the monitoring system
+# Just in case of problems, let's not hang Nagios
 $SIG{'ALRM'} = sub {
 	print "Timeout: No Answer from Client\n";
 	exit $ERRORS{'UNKNOWN'};
 };
 alarm($opt_t);
 
-my $lmstat = $utils::PATH_TO_LMSTAT ;
+#my $lmstat = $utils::PATH_TO_LMSTAT ;
+my $lmstat = dirname($opt_F);
+$lmstat = "$lmstat/lmstat";
 unless (-x $lmstat ) {
 	print "Cannot find \"lmstat\"\n";
 	exit $ERRORS{'UNKNOWN'};
@@ -167,6 +190,87 @@ while ( <CMD> ) {
 
 close CMD;
 
+#
+# bw - now loop through license file and raise expiration
+# alerts per opt_w and opt_c
+if ( ! open(FILE, "<", "$licfile") ) {
+        print "ERROR: Could not open \"$licfile\" ($!)\n";
+        exit exit $ERRORS{'UNKNOWN'};
+}
+
+# supports flexlm license file format v3.0 and later
+my $line;
+my $status = 0;
+while ( defined($line = <FILE>) ) {
+        my ($feat_name,$exp_date,$err);
+        chomp $line;
+        if ($line =~ s/\\$//) {
+                $line .= <FILE>;
+                redo unless eof(FILE);
+        }
+	next if ($line =~ /^\w*#.*/);   # ignore comment-only lines
+        # only process FEATURE, INCREMENT, or UPGRADE lines
+	if ($line =~ /^FEATURE/ || $line =~ /^INCREMENT/ ) {
+                ($feat_name,$exp_date) = ($line =~ /^\S+\s+(\S+)\s+\S+\s+\S+\s+(\S+)\s+.*$/);
+        }
+	if ($line =~ /^UPGRADE/ ) {
+                ($feat_name,$exp_date) = ($line =~ /^\S+\s+(\S+)\s+\S+\s+\S+\s+\S+\s+(\S+)\s+.*$/);
+        }
+        # compare exp_date with today's date, raise alert as specified. 
+        if ( defined($feat_name) && defined($exp_date) ) {
+                my $today = ParseDate("today");
+                my $expiry = ParseDate("$exp_date");
+                my $delta = DateCalc($today, $expiry, \$err, 0);
+                my $warndelta = ParseDateDelta("$opt_w days");
+                my $critdelta = ParseDateDelta("$opt_c days");
+                if ( Date_Cmp($delta, $warndelta) == -1 ) {
+                        if ( $status < 1 ) {
+                                $status = 1;
+                        }
+                }
+                if ( Date_Cmp($delta, $critdelta) == -1 ) {
+                        if ( $status < 2 ) {
+                                $status = 2;
+                        }
+                }
+        }
+        
+}
+
+#
+# determine overall status
+my $overall = 0;
+my $message = "";
+if ( scalar(@downsrv) == 0 && $status == 0 ) {
+        $overall = 0;
+        $message = scalar(@upsrv)." server(s) up, ".scalar(@downsrv)." server(s) down;";
+        $message = "$message no imminent expirations.";
+}
+if ( ((scalar(@upsrv) > 0) && (scalar(@downsrv) > 0)) || $status == 1 ) {
+        $overall = 1;
+        $message = scalar(@upsrv)." server(s) up, ".scalar(@downsrv)." server(s) down;";
+        if ($status == 1) {
+                $message = "$message 1 or more features expiring in fewer than $opt_w days.";
+        }
+}
+if ( ((scalar(@upsrv) <= 0) && (scalar(@downsrv) > 0)) || $status == 2 ) {
+        $overall = 2;
+        $message = scalar(@upsrv)." servers up, ".scalar(@downsrv)." servers down;";
+        if ($status == 2) {
+                $message = "$message 1 or more features expiring in fewer than $opt_c days.";
+        }
+}
+
+#
+# print check name, status, message
+print "FLEXLM ";
+if ( $overall == 0 ) { print "OK - "; }
+if ( $overall == 1 ) { print "WARNING - "; }
+if ( $overall == 2 ) { print "CRITICAL - "; }
+if ( ($overall < 0) || ($overall > 2) ) { print "UNKNOWN "; }
+print "$message ";
+
+
 if ($verbose) {
 	print "License Servers running: ".scalar(@upsrv) ."\n";
 	foreach my $upserver (@upsrv) {
@@ -200,24 +304,25 @@ if (scalar(@downsrv) > 0) {
 # perfdata
 print "\n|flexlm::up:".scalar(@upsrv).";down:".scalar(@downsrv)."\n";
 
-exit $ERRORS{'OK'} if ( scalar(@downsrv) == 0 );
-exit $ERRORS{'WARNING'} if ( (scalar(@upsrv) > 0) && (scalar(@downsrv) > 0));
+exit $ERRORS{'OK'} if ( $overall == 0 );
+exit $ERRORS{'WARNING'} if ( $overall == 1 );
+exit $ERRORS{'CRITICAL'} if ( $overall == 2 );
+exit $ERRORS{'UNKNOWN'}; # shouldn't get here!
 
 #exit $ERRORS{'OK'} if ( $servers == $lf1 + $lf2 + $lf3 );
 #exit $ERRORS{'WARNING'} if ( $servers == 3 && $lf1 + $lf2 + $lf3 == 2 );
-exit $ERRORS{'CRITICAL'};
 
 
 sub print_usage () {
 	print "Usage:
-   $PROGNAME -F <filename> [-v] [-t] [-V] [-h]
+   $PROGNAME -F <filename> [-v] [-t] [-V] [-h] [-w] [-c]
    $PROGNAME --help
    $PROGNAME --version
 ";
 }
 
 sub print_help () {
-	print_revision($PROGNAME,'@NP_VERSION@');
+	print_revision($PROGNAME,'0.2 (Yale fork)');
 	print "Copyright (c) 2000 Ernst-Dieter Martin/Karl DeBisschop
 
 Check available flexlm license managers
@@ -235,6 +340,10 @@ Check available flexlm license managers
    Show version and license information
 -h, --help
    Show this help screen
+-w, --warning=# (default = ".WARN.")
+   Warning if any feature expires in less than # days
+-c, --critical=# (default = ".CRIT.")
+   Critical if any feature expires in less than # days
 
 Flexlm license managers usually run as a single server or three servers and a
 quorum is needed.  The plugin return OK if 1 (single) or 3 (triple) servers
